d7daebd6e3474c703181c929febf148c75f26d37
iynel <ilyanalam02@gmail.com>
2026-02-03 12:28:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+AOS.init({
+  duration: 1000,
+  once: true
+});
+
+// Typing effect
+const text = "Développeuse web • Étudiante • Créative • Moderne";
+let index = 0;
+const speed = 80;
+const target = document.querySelector(".typing");
+
+function typeEffect() {
+  if (index < text.length) {
+    target.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
+  }
+}
+
+typeEffect();
